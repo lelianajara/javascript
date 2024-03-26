34b4c654b482f44544b31833a6e27ec9da902b8b
Lélia Najara <lelianajara@outlook.com>
2024-03-25 21:59:26 -0300

    Contador

---
+++ aula13/condições.js
@@ -0,0 +1,28 @@
+// 1 condição do
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+
+} while (c <= 6)
+
+
+ 2 Condição while
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+//  3 CONDIÇÃO FOR
+ // 1 parte inicio 2 parte teste 3 parte incremento
+
+ console.log(`Vai começar`)
+for (var c = 1; c <= 10; c++) {
+    console.log(`Passo ${c}`);
+    
+}
+
+console.log(`FIM`)
